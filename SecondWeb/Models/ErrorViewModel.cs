bdc5c3112f348d5e8dbf55ee4aafc831f809cbbf
volodie <119340399+volodie@users.noreply.github.com>
2023-01-31 22:24:46 +0300

    Lab15

---
+++ SecondWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace SecondWeb.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
